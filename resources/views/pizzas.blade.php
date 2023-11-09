bb14f7691a6623e2ae93c88e3d9d2a982e2c3d62
Alfie <alfiecollins44@gmail.com>
2023-11-09 22:05:09 +0000

    blade loops

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -49,25 +49,46 @@ p {
             <div class="title m-b-md">
                 Pizza List
             </div>
-            <p>{{ $type }} - {{ $base }} - {{ $price }}</p>
-            
-            @if($price > 15)
-                <p>this pizza is expensive</p>
-            @elseif($price < 5)
-                <p>pizza is cheap</p>
-            @else
-                <p>this pizza is normally priced</p>
-            @endif
+          
+{{--             @for($i = 0; $i < 5; $i++)
+                <p>the value of i is {{ $i }}</p>
+            @endfor --}}
+{{-- 
+            @for($i=0; $i<count($pizzas); $i++)
+                <p>{{ $pizzas[$i]['type'] }}</p>
+            @endfor --}}
 
-            @unless($base == 'cheesy crust')
-                <p>you don't have a cheesy crust</p>
-            @endunless
+            @foreach($pizzas as $pizza)
+                <div>
+                    {{ $loop->index+1 }}{{ $pizza['type'] }} -- {{ $pizza['base'] }}
+                    @if($loop->first)
+                        <span>- first in the loop</span>
+                    @endif
+                    @if($loop->last)
+                        <span>- last in the loop</span>
+                    @endif
+                </div>
+            @endforeach
 
-            @php
-                $name = 'alfie';
-                echo($name);
-            @endphp
         </div>
     </div>
 </body>
 </html>
+
+
+{{-- @if($price > 15)
+<p>this pizza is expensive</p>
+@elseif($price < 5)
+<p>pizza is cheap</p>
+@else
+<p>this pizza is normally priced</p>
+@endif
+
+@unless($base == 'cheesy crust')
+<p>you don't have a cheesy crust</p>
+@endunless
+
+@php
+$name = 'alfie';
+echo($name);
+@endphp --}}
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::get('/pizzas', function () {
+    $pizzas = [
+        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
+        ['type' => 'volcano', 'base' => 'garlic crust'],
+        ['type' => 'veg supreme', 'base' => 'thin and crispy']
+    ];
+    return view('pizzas', ['pizzas' => $pizzas]);
+});
+
+
+
+/* Route::get('/pizzas', function () {
     $pizza = ['type'=>'Hawaiian',
     'base' => 'garlic crust',
     'price' => 10];
     return view('pizzas', $pizza);
 });
+ */
