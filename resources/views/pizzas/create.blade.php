7912c77daa3fd9e2ffc02629b5d0ace0c4baec0b
Alfie <alfiecollins44@gmail.com>
2023-11-19 15:10:03 +0000

    changing to MVC naming conventions

---
+++ resources/views/pizzas/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.layout')
+
+@section('content')
+<div class="flex-center position-ref full-height"></div>
+        <div class="content">
+            <div class="title m-b-md">
+                Add a New Pizza
+            </div>            
+
+        </div>
+    </div>
+@endsection
