6af018b180166cd1746f82a9851e3de3357cf06c
Alfie <alfiecollins44@gmail.com>
2023-11-22 15:45:36 +0000

    accessing individual pizzas

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -20,8 +20,9 @@ class PizzaController extends Controller
     }
 
     public function show($id){
-      //Use the $id variable to query the db for a record
-      return view('pizzas.show', ['id' => $id]);
+
+        $pizza = Pizza::findOrFail($id);
+      return view('pizzas.show', ['pizza' => $pizza]);
     }
 
 
--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -1,12 +1,10 @@
 @extends('layout.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height"></div>
-        <div class="content">
-            <div class="title m-b-md">
-                {{ $id }}
-            </div>            
-
-        </div>
+    <div class="wrapper pizza-details">
+        <h1>Order for {{ $pizza->name }}</h1>
+        <p class="type">Type - {{ $pizza->type }}</p>
+        <p class="base">Base - {{ $pizza->base }}</p>
     </div>
+    <a href="/pizzas" class="back"><- Back to all Pizzas</a>
 @endsection
