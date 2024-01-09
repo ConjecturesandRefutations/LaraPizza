cdbc6134c7a246da4a1290eae0bdb8f8dfbfa35d
Alfie <alfiecollins44@gmail.com>
2024-01-09 23:10:19 +0000

    style changes

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -5,16 +5,16 @@
         <h1>Create a New Pizza</h1>
         <form action="/pizzas" method="POST">
             @csrf {{-- Cross site request forgery --}}
-            <label for="name">Your name:</label>
-            <input type="text" id="name" name="name">
-            <label for="type">Choose pizza type:</label>
+            <label for="name">Your name:&nbsp;</label>
+            <input type="text" id="name" name="name"><br/>
+            <label for="type">Choose pizza type:&nbsp;</label>
             <select name="type" id="type">
                 <option value="margherita">Margherita</option>
                 <option value="hawaiian">Hawaiian</option>
                 <option value="veg supreme">Veg Supreme</option>
                 <option value="volcano">Volcano</option>
-            </select>
-            <label for="base">Choose pizza base:</label>
+            </select><br/>
+            <label for="base">Choose pizza base:&nbsp;</label>
             <select name="base" id="base">
                 <option value="cheesy crust">Cheesy Crust</option>
                 <option value="garlic crust">Garlic Crust</option>
@@ -22,7 +22,7 @@
                 <option value="thick">Thick</option>
             </select>
             <fieldset>
-                <label>Extra Toppings</label>
+                <label>Extra Toppings</label><br/>
                 <input type="checkbox" name="toppings[]" value="mushrooms">Mushrooms<br/>
                 <input type="checkbox" name="toppings[]" value="peppers">Peppers<br/>
                 <input type="checkbox" name="toppings[]" value="garlic">Garlic<br/>
